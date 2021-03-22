6fa2e70bb6801c139a5a7131a2432ad8307bfbfd
LAPTOP-F4BMA3C0\jethr <adt107114@gm.ntcu.edu.tw>
2021-03-22 19:31:50 +0800

    20210322

---
+++ 營養HOW解/Assets/Scripts/player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    public Collider2D coll;
+    public float speed;
+    public float jumpforce;
+    public LayerMask ground;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movement();
+        SwitchAnim();
+    }
+
+    void Movement()
+    {
+        float horizontalmove=Input.GetAxis("Horizontal");
+        float facedirection=Input.GetAxisRaw("Horizontal");
+        //移動
+        if(horizontalmove!=0)
+        {
+            rb.velocity=new Vector2(horizontalmove*speed*Time.deltaTime,rb.velocity.y);
+            anim.SetFloat("running",Mathf.Abs(facedirection));
+        }
+        //方向
+        if(facedirection!=0)
+        {
+            // transform.localScale=new Vector3(facedirection*transform.localScale.x,transform.localScale.y,transform.localScale.z);
+            transform.localScale=new Vector3(facedirection*0.3f,0.3f,0.3f);
+        }
+        //跳躍
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity=new Vector2(rb.velocity.x,jumpforce*Time.deltaTime);
+            anim.SetBool("jumping",true);
+        }
+    }
+
+    void SwitchAnim()
+    {
+        anim.SetBool("idle",false);
+
+        if(anim.GetBool("jumping"))
+        {
+            if(rb.velocity.y<0)
+            {
+                anim.SetBool("jumping",false);
+                anim.SetBool("falling",true);
+            }
+        }else if(coll.IsTouchingLayers(ground))
+        {
+            anim.SetBool("falling",false);
+            anim.SetBool("idle",true);
+        }
+    }
+}
