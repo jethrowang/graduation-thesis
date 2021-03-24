e98b78da0f4aa62be7c2bea0aceedcf7a701d745
LAPTOP-F4BMA3C0\jethr <adt107114@gm.ntcu.edu.tw>
2021-03-24 23:31:50 +0800

    20210324

---
+++ 營養HOW解/Assets/Scripts/bacterial.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bacterial : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    public Transform top,buttom;
+    public float speed;
+    private float topy,buttomy;
+    public bool isup=true;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        coll=GetComponent<Collider2D>();
+        topy=top.position.y;
+        buttomy=buttom.position.y;
+        Destroy(top.gameObject);
+        Destroy(buttom.gameObject);
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if(isup)
+        {
+            rb.velocity=new Vector2(rb.velocity.x,speed);
+            if(transform.position.y>topy)
+            {
+                isup=false;
+            }
+        }
+        else
+        {
+            rb.velocity=new Vector2(rb.velocity.x,-speed);
+            if(transform.position.y<buttomy)
+            {
+                isup=true;
+            }
+        }
+    }
+}
---
+++ 營養HOW解/Assets/Scripts/virus.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class virus : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Collider2D coll;
+    public LayerMask ground;
+    public Transform leftpoint,rightpoint;
+    private bool faceleft=true;
+    public float speed,attackforce;
+    private float leftx,rightx;
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        anim=GetComponent<Animator>();
+        coll=GetComponent<Collider2D>();
+        leftx=leftpoint.position.x;
+        rightx=rightpoint.position.x;
+        Destroy(leftpoint.gameObject);
+        Destroy(rightpoint.gameObject);
+    }
+
+    void Update()
+    {
+        SwitchAnim();
+    }
+
+    void Movement()
+    {
+        if(faceleft)//面左
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                anim.SetBool("attacking",true);
+                rb.velocity=new Vector2(-speed,attackforce);
+            }
+            if(transform.position.x<leftx)
+            {
+                transform.localScale=new Vector3(-0.7f,0.7f,0.7f);
+                faceleft=false;
+            }
+        }else//面右
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                anim.SetBool("attacking",true);
+                rb.velocity=new Vector2(speed,attackforce);
+            }
+            if(transform.position.x>rightx)
+            {
+                transform.localScale=new Vector3(0.7f,0.7f,0.7f);
+                faceleft=true;
+            }
+        }
+    }
+
+    void SwitchAnim()
+    {
+        if(coll.IsTouchingLayers(ground)&&anim.GetBool("attacking"))
+        {
+            anim.SetBool("attacking",false);
+        }
+    }
+
+    void Death()
+    {
+        anim.SetTrigger("death");
+    }
+
+    public void JumpOn()
+    {
+        Destroy(gameObject);
+    }
+}
