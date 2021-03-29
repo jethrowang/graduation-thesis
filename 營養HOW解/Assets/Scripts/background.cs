0c9349ef7125a48ab783a743cf928a8f64bd8421
LAPTOP-F4BMA3C0\jethr <adt107114@gm.ntcu.edu.tw>
2021-03-29 14:37:25 +0800

    20210329

---
+++ 營養HOW解/Assets/Scripts/background.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+    public Transform cam;
+    public float moveRate;
+    private float startPointX,startPointY;
+    public bool lockY;
+    void Start()
+    {
+        startPointX=transform.position.x;
+        startPointY=transform.position.y;
+    }
+
+    void Update()
+    {
+        if(lockY)
+        {
+            transform.position=new Vector2(startPointX+cam.position.x*moveRate,transform.position.y);
+        }else
+        {
+            transform.position=new Vector2(startPointX+cam.position.x*moveRate,startPointY+cam.position.y*moveRate);
+        }
+    }
+}
